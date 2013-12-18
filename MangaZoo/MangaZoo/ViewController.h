4a37a51192ec7cfa098405fa21efbe9726f196e8
Allen17 <allenwong98@gmail.com>
2013-12-17 21:44:24 -0500

    New

---
+++ MangaZoo/MangaZoo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MangaZoo
+//
+//  Created by Allen Wong on 12/17/13.
+//  Copyright (c) 2013 Allen Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
